973954da70a835dcb4f4816228a4250f6c7d657c
RileyChampion <rchamp2u@gmail.com>
2024-11-09 13:27:19 -0500

    Structured out the FastAPI project

---
+++ app/config.py
@@ -0,0 +1,20 @@
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    # Database settings
+    DATABASE_URL: str
+
+    # Security settings
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+    # App settings
+    PROJECT_NAME: str = "User Service API"
+    API_V1_STR: str = "/api/v1"
+
+    class Config:
+        env_file = ".env"
+
+# Instantiate the settings object
+settings = Settings()
--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from app.api.v1 import routes
+from app.db.session import engine, Base
+from app.config import settings
 
-app = FastAPI()
+app = FastAPI(title= "User Service")
 
+app.include_router(routes.router, prefix="/api/v1")
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+Base.metadata.create_all(bind=engine)
